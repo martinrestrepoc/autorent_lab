bc45853ec39fa94936e01e4a0772d1c56bbbb56c
martinrestrepoc <martinrpocoral@gmail.com>
2026-03-30 09:09:31 -0500

    Se corrige error en las pruebas de integracion, debido a codigos HTTP

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { Request } from 'express';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
@@ -17,6 +25,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @HttpCode(200)
   login(@Body() body: LoginDto) {
     return this.authService.login(body.email, body.password);
   }
